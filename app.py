cd86b67089a185793c8bc3bda12e45b4f8c883b6
zeeshan40132 <zeeshan40132@gmail.com>
2025-12-23 21:16:16 +0500

    jenkins demo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Dockerized Jenkins Demo! v1'
+    return 'Welcome everyone to DevOps Presentation - Jenkins Demo'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
